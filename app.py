04b5a1984c7431b964b1b8e8a6ff4f629ba40336
ClimbsRocks <climbsbytes@gmail.com>
2016-03-02 15:42:03 -0800

    loads in files with team names, cities, and twitter handles

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request, json
+import json
+
+
 app = Flask(__name__)
+
+# list of team twitter handles back in 2010, which more closely aligns with the training data set sent over
+# http://www.sportsgeekery.com/466/the-complete-list-of-official-nfl-team-twitter-accounts/
+with open('listOf2010TeamTwitterHandles.json') as handlesFileOld:    
+    oldTeamHandles = json.load(handlesFileOld)
+
+# current list of nfl team twitter handles
+# https://twitter.com/NFL/lists/nfl-teams/members?lang=en
+with open('listOfCurrentNFLTwitterHandles.json') as handlesFile:    
+    currentTeamHandles = json.load(handlesFile)
+
+# dict where team names are the keys and city names are the values
+with open('teamNames.json') as teamNamesFile:    
+    teamNames = json.load(teamNamesFile)
+
 
 @app.route('/')
 def index():
@@ -8,9 +26,8 @@ def index():
 
 @app.route('/submitMessage',methods=['POST','GET'])
 def submitMessage():
-    # print 'heard a request for submitMessage'
-    print request.form['newMessage']
-    return request.form['newMessage']
+    message = request.form['newMessage']
+
 
 
 if __name__ == '__main__':
