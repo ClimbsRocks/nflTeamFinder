2e408319868d19e1d051bb39a11496597be90fa8
ClimbsRocks <climbsbytes@gmail.com>
2016-03-02 15:24:19 -0800

    gets form data from user in Flask server

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route('/')
+def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/submitMessage',methods=['POST','GET'])
+def submitMessage():
+    # print 'heard a request for submitMessage'
+    print request.form['newMessage']
+    return request.form['newMessage']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
