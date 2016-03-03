d3d42cd54ea6d9998b3fe7ba98773e60aa4fafb8
ClimbsRocks <climbsbytes@gmail.com>
2016-03-02 20:44:45 -0800

    displays results for user to see

--- static/js/submitNewMessage.js
+++ static/js/submitNewMessage.js
@@ -1,3 +1,11 @@
+var prettyNames = {
+  '1': 'almost certainly not',
+  '2': 'somewhat likely to be',
+  '3': 'fairly likely to be',
+  '4': 'most likely',
+  '5': 'almost certainly'
+};
+
 $(function() {
   $('.submitNewMessage').click(function() {
 
@@ -6,7 +14,7 @@ $(function() {
       data: $('form').serialize(),
       type: 'POST',
       success: function(response) {
-        console.log(response);
+        $('.result').text('This message is ' + prettyNames[response] + ' about an NFL team.');
       },
       error: function(error) {
         console.log('the error is:',error);
